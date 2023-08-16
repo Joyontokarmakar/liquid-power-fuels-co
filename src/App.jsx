3143f1b5ea7a3ea26a761c9151237605f30e30b1
Joyonto Karmakar <joyonto1997@gmail.com>
2023-08-16 20:12:37 +0600

    project created with vite-react-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className={'text-9xl text-red-500'}>Hello liquide-power-fuels-co</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
